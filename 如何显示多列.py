4294ff43660b6c2b084980c75bb7a1011973714a
chaoyue520 <huijiaowan@163.com>
2019-03-25 14:48:46 +0800

    显示多行或者多列

---
+++ 如何显示多列.py
@@ -0,0 +1,9 @@
+# 在Jupyter Notebook中
+
+
+#显示1000列
+pd.set_option('display.max_columns', 1000)
+#显示100行
+pd.set_option('display.max_rows', 100)
+#设置value的显示长度为100，默认为50
+pd.set_option('max_colwidth',10000)
